c0a084316796b44bad070c7c3bf96464fc8ff98d
gsarti <gabriele.sarti996@gmail.com>
2019-01-11 16:05:52 +0100

    Added file to perform tests

---
+++ cpp/test/BinarySearchTreeTest.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file BinarySearchTreeTest.cpp
+ * @author Gabriele Sarti
+ * @date 9 January 2019
+ * @brief Source file to perform various tests on the BinarySearchTree class.
+ */
+#include "BinarySearchTree.h"
+
+int main()
+{
+    std::pair<int, int> pair{4,4};
+    std::pair<int, int> pair2{2,2};
+    std::pair<int, int> pair3{6,6};
+    std::pair<int, int> pair4{1,1};
+    std::pair<int, int> pair5{5,5};
+    std::pair<int, int> pair6{3,3};
+    std::pair<int, int> pair7{7,7};
+    
+    BinarySearchTree<int, int> bst{pair};
+    bst.print();
+    bst.insert(pair2);
+    bst.insert(pair3);
+    bst.insert(pair4);
+    bst.print();
+    bst.insert(pair5);
+    bst.insert(pair6);
+    bst.insert(pair7);
+    bst.print();
+}
