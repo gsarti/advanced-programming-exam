04803a335404992cf4fa2574ad7f76bc6c0f8bda
Giulia Franco <giulia.franco300497@gmail.com>
2019-01-18 15:53:07 +0100

    Fixed and commented exam solution

--- Python/exam_solution.py
+++ Python/exam_solution.py
@@ -1,83 +1,132 @@
-import datetime
-import numpy as np
+"""
+Author: Giulia Franco
+Date: January 17, 2019
+Description: Module containing PostcardList class.
+"""
+
+from datetime import datetime
+
 
 class PostcardList:
+	"""
+	Class to read/write postcard messages from/to a properly
+	formatted file (date: $(DATE); from: $(SENDER); to: $(RECEIVER);)
+	"""
 	def __init__(self):
-		self._file="filein"   
-		self._postcards=[]
-		self._date={}
-		self._from={}	
-		self._to={}
-			
+		# File name from which the postcards were taken.
+		self._file = ""
+		# List of postcards read from file.
+		self._postcards = []
+		# A dictionary date,index for all postcard dates.
+		self._date = {}
+		# A dictionary string, index for all postcard senders.
+		self._from = {}
+		# A dictionary string, index for all postcard receivers.
+		self._to = {}
 
-	def writeFile(self):				#write to self._file all the current postCards recorded
-		f=open(self._file,"w")
-		for i in range(0,len(self._postcards)):
-			f.write(self._postcards[i])
-		f.close()
-	
+	def writeFile(self, filename):
+		"""
+		Writes all the recorded postcards to a file.
+		filename -> The name of the file to be written.
+		"""
+		file = open(filename, "w")
+		for postcard in self._postcards:
+			file.write(postcard)
+		file.close()
 
-	def readFile(self,p): 				#read filein and update objects
-		self.updateFile(p)
+	def readFile(self, new_file):
+		"""
+		Sets a new file, read and parse all postcards from it.
+		filename -> The name of the new file.
+		"""
+		self._postcards = []
+		self._file = new_file
+		file = open(self._file, "r")
+		for line in file:
+			self._postcards.append(line)
+		file.close()
+		self.parsePostcards()
 
-	def parsePostcards(self):			#update all dictonaries with new postcards added
-		for i in range(0,len(self._postcards)):
-			d,f,t=self._postcards[i].split(" ")
-			d=d[5:15]
-			f=f[5:-1]
-			t=t[3:-2]
-			d=datetime.datetime.strptime(d, "%Y-%m-%d")
-			if d not in self._date:
-				self._date[d]=[]
-			self._date[d].append(i)
-			if f not in self._from:
-				self._from[f]=[]
-			self._from[f].append(i)
-			if t not in self._to:
-				self._to[t]=[]
-			self._to[t].append(i)
-		del d,f,t
+	def parsePostcards(self):
+		"""
+		Updates all dictonaries from current postcards.
+		"""
+		for i in range(0, len(self._postcards)):
+			date, sender, receiver = self._postcards[i].split(" ")
+			# Remove extra characters from strings.
+			date = date[5:15]
+			sender = sender[5:-1]
+			receiver = receiver[3:-2]
+			date = datetime.strptime(date, "%Y-%m-%d")
+			if date not in self._date:
+				self._date[date] = []
+			self._date[date].append(i)
+			if sender not in self._from:
+				self._from[sender] = []
+			self._from[sender].append(i)
+			if receiver not in self._to:
+				self._to[receiver] = []
+			self._to[receiver].append(i)
 
+	def updateFile(self, filename):
+		"""
+		Updates a file appending the current postcards to it.
+		filename -> The name of the file.
+		"""
+		file = open(filename, "a")
+		for postcard in self._postcards:
+			file.write(postcard)
+		file.close()
 
-	def updateLists(self):				#update list of postcards
-		f=open(self._file,"r")
-		new=f.readlines()
-		f.close()
-		self._postcards=self._postcards+new
-		self.parsePostcards()		
+	def updateLists(self, new_file):
+		"""
+		Updates the list of postcards from new file
+		without resetting it and parses it.
+		new_file -> The new file to be used for updates.
+		"""
+		self._file = new_file
+		file = open(self._file, "r")
+		for line in file:
+			self._postcards.append(line)
+		file.close()
+		self.parsePostcards()
 
-	def updateFile(self,newfile):                   
-		self._file=newfile
-		self.updateLists()
-
-	def getNumberOfPostcards(self): 		#print current number of postcards
+	def getNumberOfPostcards(self):
+		"""
+		Prints the current number of postcards.
+		"""
 		return len(self._postcards)
 
-
-	def getPostcardsByDateRange(self,date_range):      #collect postcards within a date_range returning a list	
-		ris=[]				
+	def getPostcardsByDateRange(self, date_range):
+		"""
+		Collect postcards within a date range, returning a list.
+		date_range -> A tuple of dates.
+		"""
+		postcards_by_date = []
 		for i in self._date:
-			if date_range[0]<= i <=date_range[1]:	
+			if date_range[0] <= i <= date_range[1]:
 				for j in self._date[i]:
-					ris.append(self._postcards[j])
-		return ris
-		
+					postcards_by_date.append(self._postcards[j])
+		return postcards_by_date
 
 	def getPostcardsBySender(self, sender):
-		#collect postcards for a sender returning a list
-		ris=[]
+		"""
+		Collect postcards for a specific sender, returning a list.
+		sender -> The name of the sender to be used as filter.
+		"""
+		postcards_by_sender = []
 		if sender in self._from:
 			for i in self._from[sender]:
-				ris.append(self._postcards[i])
-		return ris
-		
+				postcards_by_sender.append(self._postcards[i])
+		return postcards_by_sender
 
-
-	def getPostcardsByReceiver(self, receiver):		#collect postcards for a receiver returning a list
-		ris=[]
+	def getPostcardsByReceiver(self, receiver):
+		"""
+		Collect postcards for a specific receiver, returning a list.
+		receiver -> The name of the receiver to be used as filter.
+		"""
+		postcards_by_receiver = []
 		if receiver in self._to:
 			for i in self._to[receiver]:
-				ris.append(self._postcards[i])
-		return ris
-
-
+				postcards_by_receiver.append(self._postcards[i])
+		return postcards_by_receiver
