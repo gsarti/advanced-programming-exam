ac5561264d244559a413dce75d280186574125aa
Giulia Franco <giulia.franco300497@gmail.com>
2019-01-22 17:18:45 +0100

    cpp test and benchmarck added

---
+++ cpp/test/Benchmarck.cpp
@@ -0,0 +1,124 @@
+/**
+ * @file Benchmarck.cpp
+ * @author Giulia Franco
+ * @date 19 January 2019
+ * @brief Source file to perform Benchmarck on the BinarySearchTree class.
+ */
+#include <memory>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+#include <map>
+#include <unordered_map>
+#include <chrono>
+#include "BinarySearchTree.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int N_max=20000; 
+//First benchmark: Define 2 BinarySearchTree of int,float. The function will measure time for find() methods for different N size
+//of the trees and print the result on file. 
+//Input: string with "int"/"float" type of pair<type,type> (for semplicity we consider just one type for couple value-key),empty BinarySearchTree.
+//Accessing element using operator[], so we expect an overestimation.
+template<class myType>
+void Timing(myType B,ofstream& myfile){
+        myfile<<"N   total time    time per operation   Log(N)"<<endl;
+        for(int j=0;j<N_max;j+=50){
+        auto begin = chrono::high_resolution_clock::now();
+                for(int i=0;i<j;++i){
+                        B.find(i);
+                }
+        auto end = chrono::high_resolution_clock::now();
+        auto total=chrono::duration_cast<chrono::microseconds>(end-begin).count();
+        myfile<<j<<"        "<<total<<"         "<<total/double(j)<<"       "<<log2(total/double(j)) <<endl;
+        }
+
+};
+
+void Bechmarck_different_type(string type){
+	ofstream myfile;
+        myfile.open ("test/Benchmarck_different_"+type+"1.txt");
+	if(type=="int"){
+		BinarySearchTree<int,int> b;
+		for(int i=0;i<N_max;++i){
+			std::pair<int, int> pair{i,i};
+			b.insert(pair);
+		}
+		cout<<"Accessing Elements for int type."<<endl;
+		myfile<<"Accessing Elements for int type."<<endl;
+		Timing(b,myfile);
+		myfile.close();
+	}
+	if(type=="double"){
+                BinarySearchTree<double,double> b;
+                vector<double> val;
+                for(int i=0;i<N_max;++i){
+			double x=((double)rand() / RAND_MAX) * (100. - 1.) + 1.;
+			pair<double, double> pair{x,x};
+                        b.insert(pair);
+			val.push_back(x);
+                }
+                cout<<"Accessing Elements for double type."<<endl;
+                myfile<<"Accessing Elements for double type."<<endl;
+		myfile<<"N   total time    time per operation   Log(N)"<<endl;
+                for(int j=0;j<N_max;j+=50){
+                        auto begin = chrono::high_resolution_clock::now();
+                        for(int i=0;i<j;++i){
+				b.find(val[i]]);
+                        }
+                        auto end = chrono::high_resolution_clock::now();
+                        auto total=chrono::duration_cast<chrono::microseconds>(end-begin).count();
+                        myfile<<j<<"        "<<total<<"         "<<total/double(j)<<"       "<<log2(total/double(j)) <<endl;
+                }
+		myfile.close();
+        }
+
+};
+
+//Comparing unbalanced BinarySearchTree, balanced BinarySearchTree,map and unordered_map.
+void Bechmarck_Map(){
+	ofstream myfile;
+        myfile.open ("test/Benchmarck_noBalance.txt");
+        BinarySearchTree<int,int> b;
+        for(int i=0;i<N_max;++i){
+        	pair<int, int> pair{i,i};
+                b.insert(pair);
+                }
+	Timing(b,myfile);
+     	myfile.close();
+	ofstream myfile1;
+	myfile1.open ("test/Benchmarck_Balance.txt");
+	b.balance();
+	Timing(b,myfile1);
+	myfile1.close();
+	map<int,int> Bench;
+	ofstream myfile2;
+        myfile2.open ("test/Benchmarck_Map.txt");
+	for(int i=0;i<N_max;++i){
+                pair<int, int> pair{i,i};
+                Bench.insert(pair);
+                }
+	Timing(Bench,myfile2);
+        myfile2.close();
+	unordered_map<int,int> UnBench;
+        ofstream myfile3;
+        myfile3.open ("test/Benchmarck_UnOrderedMap.txt");
+        for(int i=0;i<N_max;++i){
+                pair<int, int> pair{i,i};
+                UnBench.insert(pair);
+                }
+	Timing(UnBench,myfile2);
+        myfile3.close();
+};
+
+int main(){
+	
+	Bechmarck_different_type("int");
+	Bechmarck_different_type("double");
+	Bechmarck_Map();
+	return 0;
+};
